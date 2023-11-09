9c9b64c24674f27e0848d0905f14c78ca0f9ed94
VezalisPetros <petrosvez@gmail.com>
2023-11-09 17:49:59 +0200

    photos commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MainPage from './components/MainPage'
 import Stats from './components/Stats'
 import Overview from './components/Overview'
 import Features from './components/Features'
+import Photos from './components/Photos'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Stats/>
       <Overview/>
       <Features/>
+      <Photos/>
       
     </div>
   )
---
+++ src/components/Photos.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+
+const Photos = () => {
+  const [showLeftIcon, setShowLeftIcon] = useState(true);
+  const [showRightIcon, setShowRightIcon] = useState(true);
+
+  const scrollPhotos = (direction) => {
+    const carouselElement = document.querySelector('.carousel');
+    const firstImgWidth = carouselElement.firstElementChild.clientWidth + 15;
+
+    if (carouselElement) {
+      const scrollAmount = direction === 'left' ? -firstImgWidth : firstImgWidth;
+      carouselElement.scrollLeft += scrollAmount;
+    }
+  };
+
+  useEffect(() => {
+    const carouselElement = document.querySelector('.carousel');
+    if (carouselElement) {
+      setShowLeftIcon(carouselElement.scrollLeft > 0);
+      setShowRightIcon(carouselElement.scrollLeft < carouselElement.scrollWidth - carouselElement.clientWidth);
+    }
+
+    const handleScroll = () => {
+      setShowLeftIcon(carouselElement.scrollLeft > 0);
+      setShowRightIcon(carouselElement.scrollLeft < carouselElement.scrollWidth - carouselElement.clientWidth);
+    };
+
+    carouselElement.addEventListener('scroll', handleScroll);
+
+    return () => {
+      carouselElement.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className='photos-section' id='photos'>
+        
+      <div className="wrapper">
+        {showLeftIcon && (
+          <span className="material-symbols-outlined" id='left' onClick={() => scrollPhotos('left')}>
+            arrow_back_ios
+          </span>
+        )}
+        <div className="carousel">
+          <img src="/r.jpg" alt="img" />
+          <img src="/interior-modern-apartment-decorated-comfortably-with-huge-windows.jpg" alt="img" />
+          <img src="/neat-organised-workspace-with-guitar.jpg" alt="img" />
+          <img src="/wp2449378.webp" alt="img" />
+        </div>
+        {showRightIcon && (
+          <span className="material-symbols-outlined" id='right' onClick={() => scrollPhotos('right')}>
+            arrow_forward_ios
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Photos;
